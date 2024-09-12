b5643c63fbe679c1bf57c30391bc11245bd99452
Eslam Ashraf <SE.eslam.ashraf@gmail.com>
2024-09-12 08:55:09 +0300

    isocode

--- lib/src/phone_number.dart
+++ lib/src/phone_number.dart
@@ -15,10 +15,12 @@ enum PhoneNumberValidationError {
 // Extend FormzInput and provide the input type and error type.
 class PhoneNumber extends FormzInput<String, PhoneNumberValidationError> {
   // Call super.pure to represent an unmodified form input.
-  const PhoneNumber.pure() : super.pure('');
+  const PhoneNumber.pure([this.isoCode = pn_parser.IsoCode.EG]) : super.pure('');
 
   // Call super.dirty to represent a modified form input.
-  const PhoneNumber.dirty(super.value) : super.dirty();
+  const PhoneNumber.dirty(super.value, [this.isoCode = pn_parser.IsoCode.EG]) : super.dirty();
+
+  final pn_parser.IsoCode isoCode;
 
   // Override validator to handle validating a given input value.
   @override
@@ -28,7 +30,8 @@ class PhoneNumber extends FormzInput<String, PhoneNumberValidationError> {
     }
     final parsedPhoneNum = pn_parser.PhoneNumber.parse(
       phoneNum,
-      callerCountry: pn_parser.IsoCode.EG,
+      callerCountry: isoCode,
+      destinationCountry: isoCode,
     );
     if (!parsedPhoneNum.isValid()) {
       return PhoneNumberValidationError.phoneNumberInvalid;
